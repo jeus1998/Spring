53fc5e38f50a0ec30f55beabf4d01a4d70486773
jeus1998 <zeus20240228@gmail.com>
2024-05-27 16:20:17 +0900

    스프링 핵심 원리 기본편 3-2 스프링 핵심 원리 이해2 - 새로운 할인 정책 적용과 문제점

--- Spring2/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring2/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,19 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private MemberRepository memberRepository = new MemoryMemberRepository();
+    /*
+    할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다.
     private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    인터페이스에만 의존하다록 설계 변경
+     */
+    DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         int discount = discountPolicy.discount(memberRepository.findById(memberId), itemPrice);
