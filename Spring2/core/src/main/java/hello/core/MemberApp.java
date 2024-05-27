847ca6a53941af2fec997d9a14c0ddb1af2c49c7
jeus1998 <zeus20240228@gmail.com>
2024-05-27 18:17:14 +0900

    스프링 핵심 원리 기본편 3-9 스프링 핵심 원리 이해2 - 스프링으로 전환하기

--- Spring2/core/src/main/java/hello/core/MemberApp.java
+++ Spring2/core/src/main/java/hello/core/MemberApp.java
@@ -4,6 +4,8 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Junit 사용 안하고 메인 메서드로 테스트 해보기
@@ -11,11 +13,14 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
         // MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-        System.out.println("member = " + member);
         
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
