6c47eb59f9536f44bc7149e91714fdee014c29ea
jeus1998 <zeus20240228@gmail.com>
2024-05-28 17:33:40 +0900

    스프링 핵심 원리 기본편 5-5 싱글톤 컨테이너 - @Configuration & 싱글톤

--- Spring2/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring2/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -25,4 +25,9 @@ public class OrderServiceImpl implements OrderService{
         int discount = discountPolicy.discount(memberRepository.findById(memberId), itemPrice);
         return new Order(memberId, itemName, itemPrice, discount);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
