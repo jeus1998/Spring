486db8864eae075df44812129c28af903fe05112
jeus1998 <zeus20240228@gmail.com>
2024-05-28 18:19:57 +0900

    스프링 핵심 원리 기본편 5-6 싱글톤 컨테이너 - @Configuration & CGLIB 바이트코드 조작

--- Spring2/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ Spring2/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,5 +28,12 @@ public class ConfigurationSingletonTest {
 
         assertThat(memberRepository1).isSameAs(memberRepository2);
     }
-}
 
+    @Test
+    void configurationDeep(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+    }
+
+}
