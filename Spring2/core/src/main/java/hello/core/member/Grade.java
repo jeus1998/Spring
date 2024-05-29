c28c7d9e9b32d06a9d06aabef307ef92e014f9c7
jeus1998 <zeus20240228@gmail.com>
2024-05-29 20:01:28 +0900

    gitignore commit

--- Spring2/core/src/main/java/hello/core/member/Grade.java
+++
@@ -1,9 +0,0 @@
-package hello.core.member;
-
-/**
- * 회원 등급은 VIP, BASIC 2개가 존재
- */
-public enum Grade {
-    BASIC,
-    VIP
-}
--- Spring2/core/src/test/java/hello/core/scan/filter/BeanA.java
+++
@@ -1,7 +0,0 @@
-package hello.core.scan.filter;
-
-import org.springframework.stereotype.Component;
-
-@MyIncludeComponent
-public class BeanA {
-}
