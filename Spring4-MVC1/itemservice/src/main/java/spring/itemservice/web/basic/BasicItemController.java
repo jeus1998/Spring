453b6b364d439b09461ea103c5a2f9b6e2cf6b3b
jeus1998 <zeus20240228@gmail.com>
2024-06-02 21:52:38 +0900

    스프링 MVC 1편 7-5 스프링 MVC 웹 페이지 만들기 - 상품 목록 - 타임리프

---
+++ Spring4-MVC1/itemservice/src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,33 @@
+package spring.itemservice.web.basic;
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.itemservice.domain.item.Item;
+import spring.itemservice.domain.item.ItemRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
