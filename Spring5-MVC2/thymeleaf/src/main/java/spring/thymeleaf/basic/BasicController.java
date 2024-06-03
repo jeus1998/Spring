872bf71d4015c2d05d6ec257d1542681c3cb5264
jeus1998 <zeus20240228@gmail.com>
2024-06-03 11:54:51 +0900

    스프링 MVC 2편 1-5 타임리프 기본기능 - 변수 - 기본 객체들

--- Spring5-MVC2/thymeleaf/src/main/java/spring/thymeleaf/basic/BasicController.java
+++ Spring5-MVC2/thymeleaf/src/main/java/spring/thymeleaf/basic/BasicController.java
@@ -1,6 +1,10 @@
 package spring.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,13 +46,29 @@ public class BasicController {
 
         return "basic/variable";
     }
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response,
+                               HttpSession session){
 
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+
+        return "basic/basic-objects";
+    }
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
 
     @Data
     static class User{
         private String username;
         private int age;
-
         public User(String username, int age){
             this.username = username;
             this.age = age;
