a7eae0e0cab9c603021b515852ae36b28dddb4ee
jeus1998 <zeus20240228@gmail.com>
2024-06-05 20:13:48 +0900

    스프링 MVC 2편 6-12 로그인 처리1(쿠키, 세션) - 서블릿 HTTP 세션2

--- Spring5-MVC2/login/src/main/java/hello/login/web/HomeController.java
+++ Spring5-MVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -49,7 +50,7 @@ public class HomeController {
          return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -66,7 +67,19 @@ public class HomeController {
           return "loginHome";
     }
 
-
+    /**
+     * @SessionAttribute 적용해서 세션 편리하게 사용하기
+     * 세션을 생성하지는 않는다.
+     */
+    @GetMapping("/")
+    public String homeLoginV4(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        if(loginMember == null){
+             return "home";
+        }
+        model.addAttribute("member", loginMember);
+          return "loginHome";
+    }
 
 
 }
