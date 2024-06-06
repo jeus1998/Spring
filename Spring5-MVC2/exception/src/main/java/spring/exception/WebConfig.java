550da623e721a693140b35db689ffd105fbbc817
jeus1998 <zeus20240228@gmail.com>
2024-06-07 01:50:05 +0900

    스프링 MVC 2편 8-6 예외 처리와 오류 페이지 - 인터셉터

--- Spring5-MVC2/exception/src/main/java/spring/exception/WebConfig.java
+++ Spring5-MVC2/exception/src/main/java/spring/exception/WebConfig.java
@@ -5,17 +5,32 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spring.exception.filter.LogFilter;
+import spring.exception.interceptor.LogInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+    /**
+     * addInterceptors() 에서는 DispatcherType 관련 수행이 불가능하다.
+     * -> excludePatterns 에서 WebServerCustomizer 에서 만들어둔 에러 페이지 경로를 넣어준다.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
+
     /**
      * filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
      * 해당 Filter DispatcherType.REQUEST, ERROR -> 호출
      * default: REQUEST
      */
-    @Bean
+    // @Bean
     public FilterRegistrationBean<Filter> filterRegistrationBean(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
