5218b8f2148f56001515fdef5d5272dd9253bf95
jeus1998 <zeus20240228@gmail.com>
2024-06-06 18:00:04 +0900

    스프링 MVC 2편 7-2 로그인 처리2(필터, 인터셉터) - 서블릿 필터(요청로그)

---
+++ Spring5-MVC2/login/src/main/java/hello/login/web/WebConfig.java
@@ -0,0 +1,28 @@
+package hello.login.web;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+/**
+ * 필터 등록
+ * FilterRegistrationBean
+ * setFilter(등록하려는 Filter 객체)
+ * serOrder(int) 순서
+ * addUrlPatterns(경로)
+ */
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
