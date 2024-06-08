e88b0471ca08efe050264e966216adce00986ef0
jeus1998 <zeus20240228@gmail.com>
2024-06-08 13:25:17 +0900

    스프링 MVC 2편 10-9 포맷터 적용하기

--- Spring5-MVC2/typeconverter/src/main/java/spring/typeconverter/WebConfig.java
+++ Spring5-MVC2/typeconverter/src/main/java/spring/typeconverter/WebConfig.java
@@ -7,14 +7,20 @@ import spring.typeconverter.converter.IntegerToStringConverter;
 import spring.typeconverter.converter.IpPortToStringConverter;
 import spring.typeconverter.converter.StringToIntegerConverter;
 import spring.typeconverter.converter.StringToIpPortConverter;
+import spring.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-         registry.addConverter(new StringToIntegerConverter());
-         registry.addConverter(new IntegerToStringConverter());
+
+         // 주석 처리 우선순위
+         // registry.addConverter(new StringToIntegerConverter());
+         // registry.addConverter(new IntegerToStringConverter());
          registry.addConverter(new StringToIpPortConverter());
          registry.addConverter(new IpPortToStringConverter());
+
+         // 추가
+         registry.addFormatter(new MyNumberFormatter());
     }
 }
