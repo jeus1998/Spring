c503a07782cfd301c919e93bb4155a38da32e32d
jeus1998 <zeus20240228@gmail.com>
2024-06-23 22:06:13 +0900

    스프링 DB 1편 4-10 스프링과 문제 해결(트랜잭션) - 스프링 부트의 자동 리소스 등록

---
+++ Spring6-DB1/jdbc/src/test/java/spring/jdbc/service/MemberServiceV3_4Test.java
@@ -0,0 +1,110 @@
+package spring.jdbc.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import spring.jdbc.domain.Member;
+import spring.jdbc.repository.MemberRepositoryV3;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static spring.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 트랜잭션 - DataSource, TransactionManager 스프링 부트 자동 등록
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV3_4Test {
+    private static final String MEMBER_A = "memberA";
+    private static final String MEMBER_B = "memberB";
+    private static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberServiceV3_3 memberService;
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+
+    @TestConfiguration
+    @RequiredArgsConstructor
+    static class TestConfig{
+        private final DataSource dataSource;
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3(){
+            return new MemberRepositoryV3(dataSource);
+        }
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3(){
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+    @Test
+    void AopCheck(){
+        log.info("출력 memberService class={}", memberService.getClass());
+        log.info("출력 memberRepository class={}", memberRepository.getClass());
+        assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        // repository 에 @Repository 애노테이션이 붙어있고 checked 예외를 던진다면 예외 변환을 위해 프록시가 생성
+        assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    }
+    @AfterEach
+    void afterEach() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외빌생")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEX = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEX);
+
+        // when
+        assertThatThrownBy(
+                ()-> memberService.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000)
+        ).isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEX = memberRepository.findById(memberEX.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberEX.getMoney()).isEqualTo(10000);
+    }
+}
