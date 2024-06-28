a904da230b77a191d46b2db72cc27e909389535d
jeus1998 <zeus20240228@gmail.com>
2024-06-28 11:53:33 +0900

    스프링 DB 2편 5-5 데이터 접근 기술 JPA - JPA 적용1 - 개발

--- Spring7-DB2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ Spring7-DB2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity // JPA가 관리하는 객체 / RDBMS 테이블과 매핑
+// @Table(name = "item")
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA 기본 스펙
     public Item() {
     }
 
