d3b00c8ce0a61a33e31a9ccb6d0356acf967e03b
jeus1998 <zeus20240228@gmail.com>
2024-06-30 22:01:56 +0900

    스프링 DB 2편 11-1 스프링 트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ Spring7-DB2/transaction/src/test/java/spring/transaction/propagation/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package spring.transaction.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     *  memberService     @Transactional: OFF
+     *  memberRepository  @Transactional: ON
+     *  logRepository     @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success(){
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then : 모든 데이터 정상 저장
+        assertThat(memberRepository.find(username).isPresent()).isTrue();
+        assertThat(logRepository.find(username).isPresent()).isTrue();
+    }
+
+
+}
