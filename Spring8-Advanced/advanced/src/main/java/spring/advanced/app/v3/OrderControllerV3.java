4f508c3932caf4ad095b06f47052cb9c08fc62e0
jeus1998 <zeus20240228@gmail.com>
2024-07-01 16:41:27 +0900

    스프링 핵심 원리(고급) - 2-2 필드 동기화 - 적용

---
+++ Spring8-Advanced/advanced/src/main/java/spring/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,30 @@
+package spring.advanced.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.logtrace.LogTrace;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+    @GetMapping("/v3/request")
+    public String request(String itemId){
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        }
+        catch (Exception e){
+            trace.exception(status, e);
+            throw e; // 애플리케이션 흐름을 바꾸지 말자
+        }
+    }
+}
---
+++ Spring8-Advanced/advanced/src/main/java/spring/advanced/config/LogTraceConfig.java
@@ -0,0 +1,14 @@
+package spring.advanced.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.advanced.trace.logtrace.FieldLogTrace;
+import spring.advanced.trace.logtrace.LogTrace;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace(){
+        return new FieldLogTrace();
+    }
+}
