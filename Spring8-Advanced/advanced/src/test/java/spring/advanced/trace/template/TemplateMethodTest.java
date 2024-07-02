b42d37c9df2d3070616edaf4de52e8e4ebee1ff1
jeus1998 <zeus20240228@gmail.com>
2024-07-02 18:18:30 +0900

    스프링 핵심 원리(고급) - 3-3 템플릿 메서드 패턴 - 예제2

--- Spring8-Advanced/advanced/src/test/java/spring/advanced/trace/template/TemplateMethodTest.java
+++ Spring8-Advanced/advanced/src/test/java/spring/advanced/trace/template/TemplateMethodTest.java
@@ -2,6 +2,9 @@ package spring.advanced.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import spring.advanced.trace.template.code.AbstractTemplate;
+import spring.advanced.trace.template.code.SubClassLogic1;
+import spring.advanced.trace.template.code.SubClassLogic2;
 
 @Slf4j
 public class TemplateMethodTest {
@@ -29,6 +32,17 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 
 }
