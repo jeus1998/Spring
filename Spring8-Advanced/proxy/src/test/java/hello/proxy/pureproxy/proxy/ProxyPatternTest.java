3fc4582d279b516b5344ce39e1ad060965360cb0
jeus1998 <zeus20240228@gmail.com>
2024-07-03 16:30:00 +0900

    스프링 핵심 원리(고급) 4-8 프록시 패턴과 데코레이터 패턴 - 프록시 패턴 - 예제 코드2

--- Spring8-Advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ Spring8-Advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,14 @@ public class ProxyPatternTest {
     @Test
     void noProxyTest(){
         ProxyPatternClient client = new ProxyPatternClient(new RealSubject());
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+    @Test
+    void cacheProxyTest(){
+        CacheProxy cacheProxy = new CacheProxy(new RealSubject());
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
