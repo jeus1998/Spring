ff0a2ebf9dc4808d9480d6d86056d77c1bc51229
jeus1998 <zeus20240228@gmail.com>
2024-07-05 17:00:42 +0900

    스프링 핵심 원리(고급) 스프링 AOP 구현 - 예제 프로젝트 만들기

---
+++ Spring8-Advanced/aop/src/test/java/spring/aop/AopTest.java
@@ -0,0 +1,36 @@
+package spring.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring.aop.order.OrderRepository;
+import spring.aop.order.OrderService;
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+    @Test
+    void exception(){
+        assertThatThrownBy(()-> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
