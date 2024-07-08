7ab0957308ad8e1aa8c81c159bb3a764da3fcbc8
jeus1998 <zeus20240228@gmail.com>
2024-07-08 19:28:18 +0900

    스프링 핵심 원리(고급) 13-4 스프링 AOP - 실무 주의사항 - 프록시와 내부 호출 - 대안3 구조 변경

---
+++ Spring8-Advanced/aop/src/test/java/spring/aop/internalcall/CallServiceV3Test.java
@@ -0,0 +1,25 @@
+package spring.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import spring.aop.internalcall.aop.CallLogAspect;
+
+/**
+ * 스프링 부트 2.6부터는 순환 참조를 기본적으로 금지하도록 정책이 변경되었다.
+ * 이 문제를 해결하려면 application.properties : spring.main.allow-circular-references=true 추가
+ * Service1 코드 떄문에 추가
+ */
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest(properties = "spring.main.allow-circular-references=true")
+class CallServiceV3Test {
+    @Autowired
+    CallServiceV3 callServiceV3;
+    @Test
+    void external() {
+        callServiceV3.external();
+    }
+}
