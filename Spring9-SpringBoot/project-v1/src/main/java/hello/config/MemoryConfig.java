0e117f2ae9f28ffe494104a430e61b52492f70f9
jeus1998 <zeus20240228@gmail.com>
2024-07-13 19:37:40 +0900

    스프링 부트 핵심 원리와 활용 5-10 순수 라이브러리 사용하기2

---
+++ Spring9-SpringBoot/project-v1/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,18 @@
+package hello.config;
+
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MemoryConfig {
+    @Bean
+    MemoryFinder memoryFinder(){
+        return new MemoryFinder();
+    }
+    @Bean
+    MemoryController memoryController(){
+        return new MemoryController(memoryFinder());
+    }
+}
