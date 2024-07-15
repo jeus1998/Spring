4346c2d3cf0c735cfc1fae77aff16a53279e2239
jeus1998 <zeus20240228@gmail.com>
2024-07-15 16:42:46 +0900

    스프링 부트 핵심 원리와 활용 8-9 HTTP 요청 응답 기록

--- Spring9-SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
+++ Spring9-SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,7 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -9,5 +11,8 @@ public class ActuatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
     }
-
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository(){
+        return new InMemoryHttpExchangeRepository();
+    }
 }
